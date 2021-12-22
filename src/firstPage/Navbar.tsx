a53da131299bcacdb5ba8f7613ae8b963a4de6dd
Kirito <kurulluvarov@gmail.com>
2021-12-22 13:44:54 +0300

    verstka navbar succes

--- src/firstPage/Navbar.tsx
+++ src/firstPage/Navbar.tsx
@@ -3,5 +3,14 @@ import style from './Navbar.module.css'
 export let  Navbar = () =>{
 return  <div className={style.navbar}> 
      <img style={{height:'150px'}}src="https://i.gifer.com/bdg.gif" alt=""/>
+     <div className={style.navbarItems}>
+     <button>KINO</button>
+     <button>SERIALE</button>
+     <button>KRESKÓWKI</button>
+     <button>ANIME</button>
+     <button>WYBORY</button>
+     <input placeholder={'Введите название'}/>
+     </div>
+     <button>Войти</button>
 </div>
 }
